269e9b2aad4fb813ed1f5b5b24a89a6f8a4a7ee2
rkojcev <rkojcev@gmail.com>
2014-06-23 09:21:03 +0200

    cartesian path code clean up, small improvements

--- moveit_cartesian_planner/include/moveit_cartesian_planner/generate_cartesian_path.h
+++ moveit_cartesian_planner/include/moveit_cartesian_planner/generate_cartesian_path.h
@@ -16,15 +16,11 @@
 
 #include <pluginlib/class_loader.h>
 
-#include <QWidget>
 #include <QObject>
 
 
 #ifndef GENERATE_CARTESIAN_PATH_H_
 #define GENERATE_CARTESIAN_PATH_H_
-
-typedef boost::shared_ptr<move_group_interface::MoveGroup> MoveGroupPtr;
-typedef boost::shared_ptr<tf::TransformListener> TransformListenerPtr;
 
 class GenerateCartesianPath: public QObject
 {
@@ -41,27 +37,12 @@ public:
 	void init();
 public Q_SLOTS:
 	void move_to_pose(std::vector<geometry_msgs::Pose> waypoints);
-	//static void apply_simple_trajectory_filter(moveit_msgs::RobotTrajectory& trajectory);
-	//static void apply_trajectory_parabolic_time_parameterization(robot_trajectory::RobotTrajectory& rt,
-	//		moveit_msgs::RobotTrajectory &traj,unsigned int max_iterations=200,double max_time_change_per_it=.6);
 protected:
-		// generates circular trajectory above target object
-	//bool create_scan_trajectory(std::vector<geometry_msgs::Pose> &scan_poses,moveit_msgs::RobotTrajectory& scan_traj);
+
 protected:
-    // moveit
-	MoveGroupPtr move_group_ptr_;
-	TransformListenerPtr tf_listener_ptr_;
-	std::vector<geometry_msgs::Pose> scan_traj_poses_;
+
 public:
-	//remove this later its bad to have global variables this is just for initial test!!!
-	// std::string group_name_;
-	// std::string world_frame_;
-	// std::string tcp_frame_;
-	// std::string home_position_;
-	// int num_scan_points_;
-	// double reachable_scan_points_ratio_;
-	// std::string scan_topic_;
-	// bool stop_on_planning_error_;
+
 };
 
 #endif // GENERATE_CARTESIAN_PATH_H_
--- moveit_cartesian_planner/src/generate_cartesian_path.cpp
+++ moveit_cartesian_planner/src/generate_cartesian_path.cpp
@@ -14,28 +14,27 @@ const double GenerateCartesianPath::MIN_JOINT_VELOCITY = 0.01f ; // rad/sect
 
 GenerateCartesianPath::GenerateCartesianPath(QObject *parent)
 {
-	// group_name_ = "manipulator";
-	// home_position_ = "home";
-	// world_frame_ = "base_link";
-	// tcp_frame_ = "link_6";
+    //to add initializations
 }
 GenerateCartesianPath::~GenerateCartesianPath()
-{}
+{
+   //to add destructor
+   //clear points for example
+
+}
 
 void GenerateCartesianPath::init()
 {
 
-	// move_group_ptr_ = MoveGroupPtr(new move_group_interface::MoveGroup(group_name_));
-	// move_group_ptr_->setEndEffectorLink(tcp_frame_);
-	// move_group_ptr_->setPoseReferenceFrame(world_frame_);
-	//move_group_ptr_->setPlanningTime(PLANNING_TIME);
-	//tf_listener_ptr_ = TransformListenerPtr(new tf::TransformListener());
+   //to add initializations
 }
 
 void GenerateCartesianPath::move_to_pose(std::vector<geometry_msgs::Pose> waypoints)
 {
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+  //not necessary!!!
+  // ros::AsyncSpinner spinner(1);
+  // spinner.start();
+
   move_group_interface::MoveGroup group("manipulator");
   group.setNamedTarget("straight up");
   move_group_interface::MoveGroup::Plan plan;
@@ -63,65 +62,3 @@ void GenerateCartesianPath::move_to_pose(std::vector<geometry_msgs::Pose> waypoi
   group.stop();
   sleep(1.0);  // wait for stop command to be received
 }
-
-// void GenerateCartesianPath::apply_simple_trajectory_filter(	moveit_msgs::RobotTrajectory& traj)
-// {
-// 	std::vector<trajectory_msgs::JointTrajectoryPoint> &current_points = traj.joint_trajectory.points;
-// 	std::vector<trajectory_msgs::JointTrajectoryPoint> points;
-// 	double dt;
-
-// 	// setting first point timestamp to zero
-// 	current_points.front().time_from_start = ros::Duration(0);
-
-// 	// removing redundant joint points
-// 	points.push_back(current_points.front());
-// 	trajectory_msgs::JointTrajectoryPoint last_point = current_points.front();
-// 	for(unsigned int i = 1;i < current_points.size(); i++)
-// 	{
-// 		trajectory_msgs::JointTrajectoryPoint &next_point = current_points[i];
-
-// 		// time elapsed between p1 and p2
-// 		dt = (next_point.time_from_start - last_point.time_from_start).toSec();
-// 		if(dt<MIN_TRAJECTORY_TIME_STEP)
-// 		{
-// 			dt = MIN_TRAJECTORY_TIME_STEP;
-// 			next_point.time_from_start = last_point.time_from_start + ros::Duration(dt);
-// 		}
-
-// 		// computing velocity for each joint
-// 		int zero_vel_counter = 0;
-// 		next_point.velocities.resize(next_point.positions.size());
-// 		for(unsigned int j = 0; j < next_point.positions.size(); j++)
-// 		{
-// 			next_point.velocities[j] = (next_point.positions[j] - last_point.positions[j])/dt;
-// 			if(next_point.velocities[j] < MIN_JOINT_VELOCITY)
-// 			{
-// 				zero_vel_counter++;
-// 			}
-// 		}
-
-
-// 		if(zero_vel_counter < next_point.positions.size())
-// 		{
-
-// 			points.push_back(next_point);
-// 			last_point = next_point;
-// 		}
-// 	}
-
-// 	// filling first and last points with 0 velocities
-// 	points.front().velocities.assign(traj.joint_trajectory.joint_names.size(),0);
-// 	points.back().velocities.assign(traj.joint_trajectory.joint_names.size(),0);
-
-// 	// checking time stamp for last joint point
-// 	trajectory_msgs::JointTrajectoryPoint &p_last = points.back();
-// 	trajectory_msgs::JointTrajectoryPoint &p_before_last = *(points.end()-2);
-// 	if((p_last.time_from_start - p_before_last.time_from_start).toSec() < MIN_TRAJECTORY_TIME_STEP)
-// 	{
-// 		p_last.time_from_start = p_before_last.time_from_start + ros::Duration(MIN_TRAJECTORY_TIME_STEP);
-// 	}
-
-// 	traj.joint_trajectory.points.assign(points.begin(),points.end());
-
-// 	ROS_INFO_STREAM("Filtered trajectory: "<<traj);
-// }
