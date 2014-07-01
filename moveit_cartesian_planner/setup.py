72cd5d27964231d1919c72f61537721485bac836
Risto Kojcev <rkojcev@gmail.com>
2014-07-01 08:17:14 +0200

    not needed
    since we dont use phyton scripts

--- moveit_cartesian_planner/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['moveit_cartesian_planner'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
